dcf1f5448e785f7dc5ce4a7c4ec52d01015c4d83
AditiMarwah234 <104973840+AditiMarwah234@users.noreply.github.com>
2022-05-10 17:34:34 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+createCanvas(200, 200)
+}
+function draw(){
+ if (keyDown(RIGHT_ARROW)){
+background("blue")
+ }
+
+ if (keyDown(LEFT_ARROW)){
+   background("purple")
+ }
+
+ if (keyDown(UP_ARROW)) {
+   background("orange")
+ }
+
+ if (keyDown(UP_ARROW)) {
+background ("pink")
+ }
+
+}
